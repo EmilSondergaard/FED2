415acd7c68f689be0e251c622de0d8a73ff8dc54
Oliver Kjellerup Hollenbo <202107868@post.au.dk>
2023-05-04 12:39:12 +0200

    asd

---
+++ fed2/src/Components/DeleteModelFromJob.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+
+const DeleteModelFromJob = () => {
+  const [modelID, setModelID] = useState('');
+  const [jobID, setJobID] = useState('');
+
+  const handleModelIDChange = (event) => {
+    setModelID(event.target.value);
+  };
+
+  const handleJobIDChange = (event) => {
+    setJobID(event.target.value);
+  };
+
+  const handleDeleteModel = async () => {
+    try {
+      const response = await fetch(`https://localhost:7181/api/Jobs/${jobID}/Model/${modelID}`, {
+        method: 'DELETE',
+        headers: {
+            'Authorization': 'bearer eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9lbWFpbGFkZHJlc3MiOiJib3NzQG0uZGsiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJNYW5hZ2VyIiwiTW9kZWxJZCI6Ii0xIiwibmJmIjoiMTY4MzEyMzAwMCIsImV4cCI6IjE2ODMyMDk0MDAifQ.E4o0KlD4S9olPafFBnE5o2bW5dUNYCC18mCP0IoKKAM',
+          'Content-Type': 'application/json'
+        }
+      });
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+      
+    }
+  };
+
+  return (
+    <div>
+      <label htmlFor="model-id-input">Model ID:</label>
+      <input type="text" id="model-id-input" value={modelID} onChange={handleModelIDChange} />
+      <label htmlFor="job-id-input">Job ID:</label>
+      <input type="text" id="job-id-input" value={jobID} onChange={handleJobIDChange} />
+      <button onClick={handleDeleteModel}>Delete Model from Job</button>
+    </div>
+  );
+};
+
+export default DeleteModelFromJob;
